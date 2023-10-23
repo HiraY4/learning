dc01ba9352909ebf60fdc6226fde75747b9a696e
Hira Yousuf <hirayousuf26@gmail.com>
2023-10-23 14:27:18 -0700

    Initial commit

---
+++ class1/class.py
@@ -0,0 +1,19 @@
+# types => str, int, float(in point eg.7.0), boolean=>bool, list[str eg=> 'a', 'b'], 
+#tuple eg=>[name : tuple ['str', 'float', 'int'] = ('pak', 5.6, 4)
+ 
+
+
+# print("1")
+# print("2")
+# print("3")
+# print("4")
+
+# name = "Hira"
+# print(name) # name
+# print(type(name)) # type
+# print(id(name)) # physical address
+# print([i for i in dir(name) if "__" not in i]) # methods and attributes
+
+
+names: str = "Muhammad Yousuf"
+print(names)
---
+++ class2/main.py
@@ -0,0 +1,6 @@
+name: str = "ABC123 Yousuf"
+print(name.isalnum())
+print(name.count(name))
+print(name.endswith("Yousuf"))
+print(name.istitle)
+print(name.translate(name))
