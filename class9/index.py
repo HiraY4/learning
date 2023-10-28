45bcc0fbbdaeb8366d9f83898d70dc61b0d20f24
Hira Yousuf <hirayousuf26@gmail.com>
2023-10-27 17:10:11 -0700

    done

---
+++ class9/index.py
@@ -0,0 +1,12 @@
+import sys
+
+# iterative data type
+print("line1")
+print("line2") 
+
+print(sys.argv) # argv return elements in list and every element is str  
+print(type(sys.argv)) # argv type is list 
+
+# on console -> file_name space user_defined values
+# sys is part of python's core and not a part of our code
+# on console after writing - > python -> file_name(argument) -> values other (argument)  
